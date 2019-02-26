3ef3973add8adf0bf2f583ba31a85bba78918d60
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-26 18:09:33 -0500

    implemented blackjack actions

--- src/main/java/io/zipcoder/casino/CardGame/BlackJack/BlackjackActions.java
+++ src/main/java/io/zipcoder/casino/CardGame/BlackJack/BlackjackActions.java
@@ -1,22 +1,25 @@
 package io.zipcoder.casino.CardGame.BlackJack;
 
+import io.zipcoder.casino.CardGame.Cards.Deck;
+
+import java.util.function.BiConsumer;
+
 public enum BlackjackActions {
 
-    HIT(1),
-    STAND(2),
-    DOUBLEDOWN(3),
-    SPLIT(4),
-    WALKAWAY(5);
+    HIT(BlackjackPlayer::hit),
+    STAND((blackjackPlayer, deck) -> blackjackPlayer.stand()),
+    DOUBLEDOWN((blackjackPlayer, deck) -> blackjackPlayer.doubleDown()),
+    SPLIT((blackjackPlayer, deck) -> blackjackPlayer.split());
+    //WALKAWAY();
 
-    int actionValue;
+    private final BiConsumer<BlackjackPlayer, Deck> consumer;
 
-    BlackjackActions (int actionValue) {
-        this.actionValue = actionValue;
+    BlackjackActions (BiConsumer<BlackjackPlayer, Deck> consumer) {
+        this.consumer = consumer;
     }
 
-
-    public int getActionValue() {
-        return actionValue;
+    public void perform(BlackjackPlayer playerObject, Deck deckObject) {
+        consumer.accept(playerObject, deckObject);
     }
 
 
